5516b6e69e51f6aa3252091c8a774aa52c783d7f
Alexander Skrock <alexanderskrock@gmx.de>
2019-11-20 10:50:56 +0100

    changed proxy to express' solution

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
-const proxy = require('http-proxy-middleware');
+const proxy = require('express-http-proxy');
 
 const app = express();
 app.use(express.static(__dirname + '/dist/compiler-web-app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/compiler-web-app/index.html'));
 });
-var apiProxy = proxy('/api/', {
-    target: 'https://compiler-application-server.herokuapp.com/'
-});
-app.use(apiProxy);
-app.use(express.json());
+app.use('/api/', proxy('https://compiler-application-server.herokuapp.com/'));
 app.listen(process.env.PORT || 8080);
